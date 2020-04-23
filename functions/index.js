9ecf363b2d52abcc3c1c10927e8dfc5b94b2b260
alex <alexpigford@gmail.com>
2020-04-23 15:30:48 -0600

    express get and post route

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,10 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+const express = require("express");
+const app = express();
 
-exports.getChirps = functions.https.onRequest((req, res) => {
+app.get("/chirps", (req, res) => {
   admin
     .firestore()
     .collection("chirps")
@@ -25,7 +21,7 @@ exports.getChirps = functions.https.onRequest((req, res) => {
     .catch((err) => console.error(err));
 });
 
-exports.createChirp = functions.https.onRequest((req, res) => {
+app.post("/chirp", (req, res) => {
   const newChirp = {
     body: req.body.body,
     userHandle: req.body.userHandle,
@@ -44,3 +40,5 @@ exports.createChirp = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+exports.api = functions.https.onRequest(app);
