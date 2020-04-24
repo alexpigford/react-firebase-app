d2c5894976f3794a885533c5a9c3d03ceefc0ac6
alex <alexpigford@gmail.com>
2020-04-24 16:11:30 -0600

    tokens

--- functions/index.js
+++ functions/index.js
@@ -44,11 +44,49 @@ app.get("/chirps", (req, res) => {
     .catch((err) => console.error(err));
 });
 
+const FBAuth = (req, res, next) => {
+  let idToken;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer ")
+  ) {
+    idToken = req.headers.authorization.split("Bearer ")[1];
+  } else {
+    console.error("no token found");
+    return res.status(403).json({ error: "unauthorized" });
+  }
+
+  admin
+    .auth()
+    .verifyIdToken(idToken)
+    .then((decodedToken) => {
+      req.user = decodedToken;
+      console.log(decodedToken);
+      return db
+        .collection("users")
+        .where("userId", "==", req.user.uid)
+        .limit(1)
+        .get();
+    })
+    .then((data) => {
+      req.user.handle = data.docs[0].data().handle;
+      return next();
+    })
+    .catch((err) => {
+      console.error("error verifying token", err);
+      return res.status(403).json(err);
+    });
+};
+
 // post
-app.post("/chirp", (req, res) => {
+app.post("/chirp", FBAuth, (req, res) => {
+  if (req.body.body.trim() === "") {
+    return res.status(400).json({ body: "body must not be empty" });
+  }
+
   const newChirp = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString(),
   };
 
@@ -58,7 +96,7 @@ app.post("/chirp", (req, res) => {
       res.json({ message: `document ${doc.id} created successfully` });
     })
     .catch((err) => {
-      res.status(500).json({ error: "something went wrong." });
+      res.status(500).json({ error: "something went wrong" });
       console.error(err);
     });
 });
